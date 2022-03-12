af6ea1390a9f93ba8ccb6f00a9431fc4bdd868e6
Irina <golubkovairina76@gmail.com>
2022-03-12 19:37:04 +0300

    Initial commit

---
+++ src/main/java/PersonalAccount.java
@@ -0,0 +1,17 @@
+public class PersonalAccount {
+
+    int income;
+    int expenses;
+
+    public PersonalAccount(int income, int expenses) {
+        this.income = income;
+        this.expenses = expenses;
+    }
+
+    public int calculationOfTaxesOnIncome(int income) {
+
+    }
+}
+
+
+
